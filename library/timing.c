b7dc3001845dc5438dbb70e165a07e64cb41e10b
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-07-19 10:39:10 +0200

    Fix compilation for desktop with Visual Studio when targetting Win XP.

--- library/timing.c
+++ library/timing.c
@@ -54,6 +54,10 @@ struct _hr_time
 
 #endif
 
+#ifdef __MINGW32__
+#define WINAPI_FAMILY_PARTITION(x) 1
+#endif
+
 #if !defined(POLARSSL_HAVE_HARDCLOCK) && defined(POLARSSL_HAVE_ASM) &&  \
 	(defined(_MSC_VER) && defined(_M_IX86)) || defined(__WATCOMC__)
 
@@ -234,12 +238,12 @@ unsigned long get_timer( struct hr_time *val, int reset )
 
 DWORD WINAPI TimerProc( LPVOID uElapse )
 {   
-#if WINAPI_FAMILY_APP
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+    Sleep( (DWORD) uElapse );
+#else
 	HANDLE sleepEvent = CreateEventEx(NULL, NULL, CREATE_EVENT_MANUAL_RESET, EVENT_ALL_ACCESS);
 	if (!sleepEvent) return FALSE;
 	WaitForSingleObjectEx(sleepEvent, (DWORD) uElapse, FALSE);
-#else
-    Sleep( (DWORD) uElapse );
 #endif
     alarmed = 1; 
     return( TRUE );
@@ -256,12 +260,12 @@ void set_alarm( int seconds )
 
 void m_sleep( int milliseconds )
 {
-#if WINAPI_FAMILY_APP
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+    Sleep( milliseconds );
+#else
 	HANDLE sleepEvent = CreateEventEx(NULL, NULL, CREATE_EVENT_MANUAL_RESET, EVENT_ALL_ACCESS);
 	if (!sleepEvent) return;
 	WaitForSingleObjectEx(sleepEvent, milliseconds, FALSE);
-#else
-    Sleep( milliseconds );
 #endif
 }
 
